5983611540cb857e0f51f7b4620a8a04d352734d
ensaro <41113027+ensarokumus@users.noreply.github.com>
2024-01-15 15:10:06 +1100

    Create first React test

--- src/tests/App.test.jsx
+++ src/tests/App.test.jsx
@@ -1,27 +1,9 @@
-import { describe, it, expect } from "vitest";
-
-describe("something truthy and falsy", () => {
-  it("true to be true", () => {
-    expect(true).toBe(true);
-  });
-
-  it("false to be false", () => {
-    expect(false).toBe(false);
-  });
-});
-
 import { render, screen } from "@testing-library/react";
-
 import App from "../App.jsx";
 
-<App className="jsx"></App>;
-
-describe("App", () => {
-  it("renders headline", () => {
-    render(<App title="React" />);
-
-    screen.debug();
-
-    // check if App components renders headline
+describe("App component", () => {
+  it("renders correct heading", () => {
+    render(<App />);
+    expect(screen.getByRole("heading").textContent).toMatch(/our first test/i);
   });
 });
