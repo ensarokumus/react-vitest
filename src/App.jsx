77c5ac9ad70cde9822fab4b6a9ac5bd9052a9ef9
ensaro <41113027+ensarokumus@users.noreply.github.com>
2024-01-16 18:00:58 +1100

    Add an async test example

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
-  return <h1>Our First Test</h1>;
+  const [heading, setHeading] = useState("Magnificent Monkeys");
+
+  const clickHandler = () => {
+    setHeading("Radical Rhinos");
+  };
+
+  return (
+    <>
+      <button type="button" onClick={clickHandler}>
+        Click Me
+      </button>
+      <h1>{heading}</h1>
+    </>
+  );
 }
 
 export default App;
--- src/tests/App.test.jsx
+++ src/tests/App.test.jsx
@@ -1,9 +1,22 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import App from "../App.jsx";
 
 describe("App component", () => {
-  it("renders correct heading", () => {
+  it("renders magnificant monkeys", () => {
+    const { container } = render(<App />);
+    expect(container).toMatchSnapshot();
+  });
+
+  it("renders radical rhinos after button click", async () => {
+    const user = userEvent.setup();
+
     render(<App />);
-    expect(screen.getByRole("heading").textContent).toMatch(/our first test/i);
+    const button = screen.getByRole("button", { name: "Click Me" });
+
+    await user.click(button);
+
+    expect(screen.getByRole("heading").textContent).toMatch(/radical rhinos/i);
   });
 });
+
